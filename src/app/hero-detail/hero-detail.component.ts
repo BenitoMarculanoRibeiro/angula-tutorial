f1f57f2d6e134083240e32261b3b5880c8084e3f
BenitoMarculanoRibeiro <benito2016mr@gmail.com>
2022-06-27 15:07:54 -0300

    0.0.5 - Modificando heroes-details.

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Hero } from '../hero.model';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.scss']
 })
-export class HeroDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HeroDetailComponent {
+  @Input() hero?: Hero;
 }
