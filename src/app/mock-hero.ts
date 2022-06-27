9ab41b7f9492ad344aceadd537c5b757ee9b56c0
BenitoMarculanoRibeiro <benito2016mr@gmail.com>
2022-06-27 10:06:29 -0300

    0.0.4 - Muiti heroes.

---
+++ src/app/mock-hero.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Wolverine' },
+    { id: 2, name: 'Homem Aranha' },
+    { id: 3, name: 'Batman' },
+    { id: 4, name: 'Capa Amarela' },
+    { id: 5, name: 'Saitama' },
+    { id: 6, name: 'Genos' },
+    { id: 7, name: 'Patrick' },
+    { id: 8, name: 'Baki' },
+    { id: 9, name: 'Yugiro' },
+    { id: 10, name: 'Ala' }
+];
